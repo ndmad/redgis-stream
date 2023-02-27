c173bcfab55e9cad82bea3c3e367e8998760c504
ndmad <redgisafrica@gmail.com>
2023-02-27 20:54:01 +0000

    ajout d'un titre

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+st.title("Application de Distribution normale")
+st.subheader("Auteur : Mamadou NDIAYE")
+st.write(
+    ("Cette application permet d'afficher l'histogramme d'une distribution normale."
+     " L'utilisateur a la possibilité de varier le nombre de bins de l'histogramme"
+     " et de donner un titre au graphique.")
+)
 data = np.random.normal(size=1000)
 data = pd.DataFrame(data, columns=["Dist_norm"])
 st.write(data.head())
